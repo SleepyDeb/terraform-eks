51a6bd2d5f9e6d51f5907cc3beee3ff5f83d70a3
Alessio DM <alessio.dimaria@gmail.com>
2025-06-15 18:13:43 +0200

    Adding internet access

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,6 +3,28 @@ resource "aws_vpc" "this" {
   tags       = merge(var.tags, { Name = var.name })
 }
 
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags   = merge(var.tags, { Name = "${var.name}-igw" })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  tags   = merge(var.tags, { Name = "${var.name}-public-rt" })
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnets)
   vpc_id                  = aws_vpc.this.id
