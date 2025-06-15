bdb6bfc7d0ae3453d5c072c0df1cfe3fadc0cb67
Alessio DM <alessio.dimaria@gmail.com>
2025-06-15 18:35:31 +0200

    private subnets needs internet access too

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -25,6 +25,18 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route" "private_internet_access" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnets)
   vpc_id                  = aws_vpc.this.id
